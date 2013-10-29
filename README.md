22e66ae58692c9f9bdf86e28c94fa68d1d0224f5
nickcen <cenyongh@gmail.com>
2013-10-29 17:32:46 +0800

    Levenshtein_distance method

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ algos
 common algorithms in ruby
 
 1. [Levenshtein_distance](http://en.wikipedia.org/wiki/Levenshtein_distance)
+
+    StringAlgo::levenshtein_distance(str_1, str_2)
+
--- spec/string_algo_spec.rb
+++ spec/string_algo_spec.rb
@@ -2,48 +2,44 @@ require 'spec_helper'
 
 module Algos
   describe StringAlgo do
-    let :algo do 
-      StringAlgo.new
-    end
-
     it "should return 0 for the same string" do
       a = 'abc'
-      algo.levenshtein_distance(a, a).should == 0
+      StringAlgo::levenshtein_distance(a, a).should == 0
     end
 
     it "should return the length of the non empty string" do
       a = 'abc'
-      algo.levenshtein_distance(a, '').should == a.length
+      StringAlgo::levenshtein_distance(a, '').should == a.length
     end
 
     it "should return 1 for insertion" do
       a = 'abc'
       b = 'abcd'
-      algo.levenshtein_distance(a, b).should == 1
+      StringAlgo::levenshtein_distance(a, b).should == 1
     end
 
     it "should return 1 for insertion" do
       a = 'abc'
       b = 'bc'
-      algo.levenshtein_distance(a, b).should == 1
+      StringAlgo::levenshtein_distance(a, b).should == 1
     end
 
     it "should return 1 for modification" do
       a = 'abc'
       b = 'abd'
-      algo.levenshtein_distance(a, b).should == 1
+      StringAlgo::levenshtein_distance(a, b).should == 1
     end
 
     it "should return 1 for modification" do
       a = 'abc'
       b = 'adc'
-      algo.levenshtein_distance(a, b).should == 1
+      StringAlgo::levenshtein_distance(a, b).should == 1
     end
 
     it "should return 3 for totally different" do
       a = 'abc'
       b = 'def'
-      algo.levenshtein_distance(a, b).should == 3
+      StringAlgo::levenshtein_distance(a, b).should == 3
     end
   end
 end
